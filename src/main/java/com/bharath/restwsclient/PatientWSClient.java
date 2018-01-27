725a492d5d946b4704f99e3e1f10a2a471db9f96
beny <jlbeny100@gmail.com>
2018-01-27 23:16:33 +0100

    pass request paramets dynamically

--- src/main/java/com/bharath/restwsclient/PatientWSClient.java
+++ src/main/java/com/bharath/restwsclient/PatientWSClient.java
@@ -10,10 +10,12 @@ import com.bharath.restwsclient.model.Patient;
 
 public class PatientWSClient {
 	
+	private static final String PATIENT_SERVICE_URL = "http://localhost:8080/restws/services/patientservice";
+
 	public static void main(String[] args) {
 		Client client = ClientBuilder.newClient();
 		
-		WebTarget target = client.target("http://localhost:8080/restws/services/patientservice/patients/123");
+		WebTarget target = client.target(PATIENT_SERVICE_URL).path("/patients").path("/{id}").resolveTemplate("id", 123);
 		Builder request = target.request();
 		
 		// obtenemos objeto response de manera generica
